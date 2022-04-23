081dc9aac22276906b44b8cb2b50dc1b74c547b7
nahyunLee <sksms304@gmail.com>
2022-04-23 20:12:51 +0900

    project init

---
+++ src/test/java/nahyun/test_java/TestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package nahyun.test_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
